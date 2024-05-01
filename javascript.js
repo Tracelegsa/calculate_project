08b1be15122ae16febd28fddedfdcc98558692ff
Tracelegsa <65673118+Tracelegsa@users.noreply.github.com>
2024-05-01 15:50:20 +0900

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,37 @@
+var numberClicked = false;
+function add (char) {
+    if (numberClicked == false) {
+        if (isNaN(char) == false) {
+
+        }   else {
+            document.getElementById('display').value += char;
+        }
+    } else {
+        document.getElementById('display').value += char;
+    }
+
+    if(isNaN(char) == true) {
+        numberClicked = false;
+    } else {
+        numberClicked = true;
+    }
+}
+
+
+
+
+function add (char) {
+    var display = document.getElementById('display');
+    display.value = display.value + char;
+}
+
+function calculate() {
+    var display = document.getElementById('display');
+    var result = eval(display.value);
+    document.getElementById('result').value = result;
+}
+
+function reset() {
+    document.getElementById('display').value = "";
+    document.getElementById('result').value = "";
+}
